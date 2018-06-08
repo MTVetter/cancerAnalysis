89e95011d4ba6f24d214842fd09dff1c789abfc4
MTVetter <mvetter2@wisc.edu>
2018-06-08 08:59:56 -0500

    initial Python code

---
+++ cancerAnalysis.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+
+#Setting up the workspace
+env.workspace = "U:\School\Geog777\Project1\Workspace"
+
+#Creating the GUI
+root = Tk()
+root.title("Relationship between Cancer and Nitrate Levels")
+root.resizable(width = FALSE, height = FALSE)
+root.geometry("1500x900")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
